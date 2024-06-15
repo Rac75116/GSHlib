3fdfbf40561ff7b6c31bccb009b47e4009112930
sortA0329 <tsouta0329@gmail.com>
2024-06-15 20:57:37 +0900

    update

--- Test.cpp
+++ Test.cpp
@@ -4,51 +4,27 @@
 //#include <cmath>
 #include <gsh/InOut.hpp>
 #include <gsh/TypeDef.hpp>
-#include <gsh/Algorithm.hpp>
-#include <gsh/Vec.hpp>
-#include <gsh/Arr.hpp>
 #include <gsh/Random.hpp>
 #include <gsh/Timer.hpp>
-#include <gsh/Numeric.hpp>
 #include <gsh/Macro.hpp>
-#include <gsh/Util.hpp>
-#include <gsh/DisjointSet.hpp>
 #include <iostream>
 #include <cassert>
 #include <sstream>
 
 int main() {
     try {
-        [[maybe_unused]] gsh::Rand32 engine;
+        //[[maybe_unused]] gsh::Rand64 engine;
         {
             using namespace std;
             using namespace gsh;
             using namespace itype;
             using namespace ftype;
-            /*
-            ostringstream oss;
-            u64 cor = 0;
-            REP(i, 100000000) {
-                u32 tmp = engine() / 150000;
-                cor += tmp;
-                oss << tmp << ' ';
+            BasicReader r;
+            u32 N = Parser<u32>{}(r);
+            for (u32 i = 0; i != N; ++i) {
+                u64 A = Parser<u64>{}(r), B = Parser<u64>{}(r);
+                printf("%llu\n", A + B);
             }
-            oss << "              ";
-            std::cout << cor << std::endl;
-            string s = oss.str();
-            StringReader r(s.data(), s.size());
-            ClockTimer t;
-            u64 res = 0;
-            REP(i, 100000000) {
-                res += Parser<i16>{}(r);
-            }
-            t.print();
-            cout << res << endl;
-            */
-            Rand32 engine;
-            //for (u32 i = 0; i != 1000000000; ++i) engine();
-            engine.discard(1000000000);
-            std::cout << engine() << std::endl;
         }
     } catch (gsh::Exception& e) {
         std::cerr << e.what() << std::endl;
--- gsh/InOut.hpp
+++ gsh/InOut.hpp
@@ -1,10 +1,13 @@
 #pragma once
-#include <bit>
-#include <cstring>          // std::memset, std::memcpy, std::memmove
-#include <unistd.h>         // read
+#include <bit>        // std::countr_zero
+#include <exception>  // std::terminate
+#include <cstring>    // std::memset, std::memcpy, std::memmove
+#include <unistd.h>   // read
+#ifndef _WIN32
+#include <sys/mman.h>  // mmap
+#include <sys/stat.h>  // stat, fstat
+#endif
 #include <gsh/TypeDef.hpp>  // gsh::itype, gsh::ctype
-#include <iostream>
-#include <cstdint>
 
 namespace gsh {
 
@@ -17,7 +20,7 @@ namespace internal {
         v ^= 0x3030303030303030;
         itype::i32 tmp = std::countr_zero(v & 0xf0f0f0f0f0f0f0f0) >> 3;
         v <<= (64 - (tmp << 3));
-        stream.bump(tmp + 1);
+        stream.skip(tmp + 1);
         v = (v * 10 + (v >> 8)) & 0x00ff00ff00ff00ff;
         v = (v * 100 + (v >> 16)) & 0x0000ffff0000ffff;
         v = (v * 10000 + (v >> 32)) & 0x00000000ffffffff;
@@ -33,7 +36,7 @@ namespace internal {
                 v = (v * 100 + (v >> 16)) & 0x0000ffff0000ffff;
                 v = (v * 10000 + (v >> 32)) & 0x00000000ffffffff;
                 res = v;
-                stream.bump(8);
+                stream.skip(8);
             }
         }
         itype::u64 buf;
@@ -45,7 +48,7 @@ namespace internal {
                 v = (v * 10 + (v >> 8)) & 0x00ff00ff;
                 v = (v * 100 + (v >> 16)) & 0x0000ffff;
                 res = 10000 * res + v;
-                stream.bump(4);
+                stream.skip(4);
             }
         }
         {
@@ -54,61 +57,143 @@ namespace internal {
                 buf >>= 16;
                 v = (v * 10 + (v >> 8)) & 0x00ff;
                 res = 100 * res + v;
-                stream.bump(2);
+                stream.skip(2);
             }
         }
         {
             const ctype::c8 v = ctype::c8(buf) ^ 0x30;
             const bool f = !(v & 0xf0);
             res = f ? 10 * res + v : res;
-            stream.bump(f + 1);
+            stream.skip(f + 1);
         }
         return res;
     };
+    template<class Stream> constexpr itype::u64 Parseu64(Stream& stream) {
+        itype::u64 res = 0;
+        {
+            itype::u64 v;
+            std::memcpy(&v, stream.current(), 8);
+            if (!((v ^= 0x3030303030303030) & 0xf0f0f0f0f0f0f0f0)) {
+                stream.skip(8);
+                itype::u64 u;
+                std::memcpy(&u, stream.current(), 8);
+                if (!((u ^= 0x3030303030303030) & 0xf0f0f0f0f0f0f0f0)) {
+                    v = (v * 10 + (v >> 8)) & 0x00ff00ff00ff00ff;
+                    u = (u * 10 + (u >> 8)) & 0x00ff00ff00ff00ff;
+                    v = (v * 100 + (v >> 16)) & 0x0000ffff0000ffff;
+                    u = (u * 100 + (u >> 16)) & 0x0000ffff0000ffff;
+                    v = (v * 10000 + (v >> 32)) & 0x00000000ffffffff;
+                    u = (u * 10000 + (u >> 32)) & 0x00000000ffffffff;
+                    res = v * 100000000 + u;
+                    stream.skip(8);
+                } else {
+                    v = (v * 10 + (v >> 8)) & 0x00ff00ff00ff00ff;
+                    v = (v * 100 + (v >> 16)) & 0x0000ffff0000ffff;
+                    v = (v * 10000 + (v >> 32)) & 0x00000000ffffffff;
+                    res = v;
+                }
+            }
+        }
+        itype::u64 buf;
+        std::memcpy(&buf, stream.current(), 8);
+        {
+            itype::u32 v = buf;
+            if (!((v ^= 0x30303030) & 0xf0f0f0f0)) {
+                buf >>= 32;
+                v = (v * 10 + (v >> 8)) & 0x00ff00ff;
+                v = (v * 100 + (v >> 16)) & 0x0000ffff;
+                res = 10000 * res + v;
+                stream.skip(4);
+            }
+        }
+        {
+            itype::u16 v = buf;
+            if (!((v ^= 0x3030) & 0xf0f0)) {
+                buf >>= 16;
+                v = (v * 10 + (v >> 8)) & 0x00ff;
+                res = 100 * res + v;
+                stream.skip(2);
+            }
+        }
+        {
+            const ctype::c8 v = ctype::c8(buf) ^ 0x30;
+            const bool f = !(v & 0xf0);
+            res = f ? 10 * res + v : res;
+            stream.skip(f + 1);
+        }
+        return res;
+    }
 }  // namespace internal
 
 template<> class Parser<itype::u16> {
 public:
     template<class Stream> constexpr itype::u16 operator()(Stream& stream) const {
-        if (!stream.reload(8)) return 0;
+        stream.reload(8);
         return internal::Parseu16(stream);
     }
 };
 template<> class Parser<itype::i16> {
 public:
     template<class Stream> constexpr itype::i16 operator()(Stream& stream) const {
-        if (!stream.reload(9)) return 0;
-        const bool neg = *stream.current() == '-';
-        stream.bump(neg);
+        stream.reload(9);
+        bool neg = *stream.current() == '-';
+        if (neg) stream.skip(1);
         itype::i16 tmp = internal::Parseu16(stream);
-        return neg ? -tmp : tmp;
+        if (neg) tmp = -tmp;
+        return tmp;
     }
 };
 template<> class Parser<itype::u32> {
 public:
     template<class Stream> constexpr itype::u32 operator()(Stream& stream) const {
-        if (!stream.reload(16)) return 0;
+        stream.reload(16);
         return internal::Parseu32(stream);
     }
 };
 template<> class Parser<itype::i32> {
 public:
     template<class Stream> constexpr itype::i32 operator()(Stream& stream) const {
-        if (!stream.reload(16)) return 0;
-        const bool neg = *stream.current() == '-';
-        stream.bump(neg);
-        const itype::u32 tmp = internal::Parseu32(stream);
-        return neg ? -tmp : tmp;
+        stream.reload(16);
+        bool neg = *stream.current() == '-';
+        if (neg) stream.skip(1);
+        itype::i32 tmp = internal::Parseu32(stream);
+        if (neg) tmp = -tmp;
+        return tmp;
+    }
+};
+template<> class Parser<itype::u64> {
+public:
+    template<class Stream> constexpr itype::u64 operator()(Stream& stream) const {
+        stream.reload(32);
+        return internal::Parseu64(stream);
+    }
+};
+template<> class Parser<itype::i64> {
+public:
+    template<class Stream> constexpr itype::i64 operator()(Stream& stream) const {
+        stream.reload(32);
+        bool neg = *stream.current() == '-';
+        if (neg) stream.skip(1);
+        itype::i64 tmp = internal::Parseu64(stream);
+        if (neg) tmp = -tmp;
+        return tmp;
     }
 };
 
+template<class T> class Formatter;
+
+template<> class Formatter<itype::u32> {
+    template<class Stream> constexpr itype::u32 operator()(Stream& stream) const { stream.flush(16); }
+};
+
 class BasicReader {
-private:
     constexpr static itype::u32 Bufsize = 1 << 18;
+    const itype::i32 fh = 0;
     ctype::c8 buf[Bufsize + 1];
     ctype::c8 *cur = buf, *eof = buf;
 public:
-    BasicReader() { cur = buf, eof = buf; }
+    BasicReader() {}
+    BasicReader(itype::i32 filehandle) : fh(filehandle) {}
     BasicReader(const BasicReader& rhs) {
         std::memcpy(buf, rhs.buf, rhs.eof - rhs.cur);
         cur = buf + (rhs.cur - rhs.buf);
@@ -120,37 +205,47 @@ public:
         eof = buf + (rhs.cur - rhs.eof);
         return *this;
     }
-    bool reload() {
-        itype::u32 rem = eof - cur;
-        std::memmove(buf, cur, rem);
-        *(eof = buf + rem + read(0, buf + rem, Bufsize - rem)) = '\0';
-        cur = buf;
-        return true;
+    void reload() {
+        if (eof == buf + Bufsize) [[likely]] {
+            itype::u32 rem = eof - cur;
+            std::memmove(buf, cur, rem);
+            *(eof = buf + rem + read(fh, buf + rem, Bufsize - rem)) = '\0';
+            cur = buf;
+        } else if (eof == cur) {
+            *(eof = buf + read(fh, buf, Bufsize)) = '\0';
+            cur = buf;
+        }
     }
-    bool reload(itype::u32 len) {
-        if (avail() < len) [[unlikely]] {
+    void reload(itype::u32 len) {
+        if (avail() < len) [[unlikely]]
             reload();
-            return avail() >= len;
-        } else return true;
     }
     itype::u32 avail() const { return eof - cur; }
     const ctype::c8* current() const { return cur; }
-    void bump(itype::u32 n) { cur += n; }
+    void skip(itype::u32 n) { cur += n; }
 };
-
-class StringReader {
-private:
-    const ctype::c8 *cur = nullptr, *eof = nullptr;
+class MmapReader {
+    const itype::i32 fh;
+    ctype::c8* buf;
+    ctype::c8 *cur, *eof;
 public:
-    constexpr StringReader() {}
-    constexpr StringReader(const char* c, itype::u32 n) : cur(c), eof(c + n) {}
-    constexpr StringReader(const StringReader&) = default;
-    constexpr StringReader& operator=(const StringReader&) = default;
-    constexpr bool reload() { return true; }
-    constexpr bool reload(itype::u32) { return true; }
-    constexpr itype::u32 avail() const { return eof - cur; }
-    constexpr const ctype::c8* current() const { return cur; }
-    void bump(itype::u32 n) { cur += n; }
+    MmapReader() : fh(0) {
+#ifdef _WIN32
+        write(1, "gsh::MmapReader / gsh::MmapReader is not available for Windows.\n", 64);
+        std::terminate();
+#else
+        struct stat st;
+        fstat(0, &st);
+        buf = reinterpret_cast<ctype::c8*>(mmap(nullptr, st.st_size + 64, PROT_READ, MAP_PRIVATE, 0, 0));
+        cur = buf;
+        eof = buf + st.st_size;
+#endif
+    }
+    void reload() const {}
+    void reload(itype::u32) const {}
+    itype::u32 avail() const { return eof - cur; }
+    const ctype::c8* current() const { return cur; }
+    void skip(itype::u32 n) { cur += n; }
 };
 
 }  // namespace gsh

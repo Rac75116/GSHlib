0828690de3c285eff436e4d02b4a64fe172d3902
sortA0329 <tsouta0329@gmail.com>
2024-07-02 10:00:30 +0900

    update

--- gsh/Geometry.hpp
+++ gsh/Geometry.hpp
@@ -28,28 +28,16 @@ template<Rangeof<Point2<itype::i32>> T> Arr<Point2<itype::i32>> ArgumentSort(T&&
     for (itype::u32 i = 0; auto& p : r) {
         auto [x, y] = p;
         itype::u64 ord = 0;
-        constexpr itype::u64 mx = ((1ull << 61) - 1);
-        if (x >= y) {
-            if (x == 0) {
-                if (y == 0) ord = 4ull << 61;
-                else ord = 2ull << 61;
-            } else {
-                itype::i64 tmp = static_cast<itype::i128>(y) * mx / x;
-                if (x < 0) ord = (1ull << 61) | tmp;
-                else {
-                    if (y >= 0) ord = (4ull << 61) | tmp;
-                    else ord = (3ull << 61) | (mx - static_cast<itype::u64>(-tmp));
-                }
-            }
-        } else {
-            if (y == 0) ord = 7ull << 61;
-            else {
-                itype::i64 tmp = static_cast<itype::i128>(-x) * mx / y;
-                if (y > 0) {
-                    if (x > 0) ord = (5ull << 61) | (mx - static_cast<itype::u64>(-tmp));
-                    else ord = (6ull << 61) | tmp;
-                } else ord = (mx - static_cast<itype::u64>(-tmp));
-            }
+        const bool xs = (x >= 0), ys = (y >= 0);
+        const itype::u64 xu = (xs ? x : -x), yu = (ys ? y : -y);
+        const itype::u64 mx = (xu < yu ? yu : xu), mn = (xu < yu ? xu : yu);
+        const bool rev = (xs ^ ys) ^ (xu < yu);
+        const itype::u64 id = ys * 4ull + (xs ^ ys) * 2ull + rev;
+        if (x == 0 && y == 0) ord = 4ull << 61;
+        else {
+            constexpr itype::u64 li = (1ull << 61) - 1;
+            const itype::u64 tmp = (static_cast<itype::u128>(mn) * li) / mx;
+            ord = id << 61 | (rev ? li - tmp : tmp);
         }
         v[i++] = static_cast<itype::u128>(std::bit_cast<itype::u64>(p)) << 64 | ord;
     }

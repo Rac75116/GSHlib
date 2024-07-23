a2c510523e11631130ee2782d532124017dadee0
sortA0329 <tsouta0329@gmail.com>
2024-07-23 22:34:32 +0900

    update

--- Test.cpp
+++ Test.cpp
@@ -9,7 +9,6 @@
 #endif
 #include <gsh/InOut.hpp>
 #include <gsh/Exception.hpp>
-#include <gsh/Modint.hpp>
 
 #if false
 #include <fcntl.h>
@@ -25,28 +24,6 @@ void Main() {
     using namespace gsh::itype;
     using namespace gsh::ftype;
     using namespace gsh::ctype;
-    /*
-    u32 T = Parser<u8dig>{}(r).val;
-    while (T--) {
-        i128 A = Parser<i128>{}(r), B = Parser<i128>{}(r);
-        Formatter<i128>{}(w, A + B);
-        Formatter<c8>{}(w, '\n');
-    }
-    */
-    using mint = StaticModint32<1000000007>;
-    u64 L = Parser<u64>{}(r), R = Parser<u64>{}(r);
-    u64 k = 1;
-    mint res = 0;
-    for (u32 i = 1; i <= 18; ++i) {
-        u64 l = k * 10 - 1;
-        if (!(l < L || k > R)) {
-            u64 a = k < L ? L : k;
-            u64 b = l < R ? l : R;
-            res += mint(i) * (mint(a) + mint(b)) * mint(b - a + 1) / 2;
-        }
-        k *= 10;
-    }
-    Formatter<u32>{}(w, res.val());
 }
 int main() {
 #ifdef ONLINE_JUDGE

7778f07ddef866dc5bf34d8244252ddf58e95d1c
Rac <rac75116@gmail.com>
2025-09-14 01:27:44 +0900

    update

--- gsh/Annealing.hpp
+++ gsh/Annealing.hpp
@@ -10,7 +10,6 @@ namespace gsh {
 
 class ZeroTemp {
 public:
-    using result_type = ftype::f64;
     ZeroTemp() {}
     ftype::f64 operator()([[maybe_unused]] ftype::f64 progress) const { return 0.0; }
 };
@@ -18,7 +17,6 @@ public:
 class LinearTemp {
     ftype::f64 init_temp, final_temp;
 public:
-    using result_type = ftype::f64;
     LinearTemp(ftype::f64 init_temp, ftype::f64 final_temp = 0.0) : init_temp(init_temp), final_temp(final_temp) {}
     ftype::f64 operator()(ftype::f64 progress) const { return init_temp + (final_temp - init_temp) * progress; }
 };
@@ -27,7 +25,6 @@ class ExponentialTemp {
     ftype::f64 init_temp;
     ftype::f64 ratio;
 public:
-    using result_type = ftype::f64;
     ExponentialTemp(ftype::f64 init_temp, ftype::f64 final_temp = 0.001) : init_temp(init_temp), ratio(final_temp / init_temp) {}
     ftype::f64 operator()(ftype::f64 progress) const { return init_temp * std::pow(ratio, progress); }
 };
@@ -37,7 +34,6 @@ class VariableExponentialTemp {
     ftype::f64 shape;
     ftype::f64 ratio;
 public:
-    using result_type = ftype::f64;
     VariableExponentialTemp(ftype::f64 init_temp, ftype::f64 final_temp = 0.001, ftype::f64 shape = 2.0) : init_temp(init_temp), shape(shape), ratio(final_temp / init_temp) {}
     ftype::f64 operator()(ftype::f64 progress) const { return init_temp * std::pow(ratio, std::pow(progress, shape)); }
 };
@@ -46,7 +42,6 @@ class SigmoidTemp {
     ftype::f64 init_temp, final_temp;
     ftype::f64 steepness;
 public:
-    using result_type = ftype::f64;
     SigmoidTemp(ftype::f64 init_temp, ftype::f64 final_temp = 0.0, ftype::f64 steepness = 10.0) : init_temp(init_temp), final_temp(final_temp), steepness(steepness) {}
     ftype::f64 operator()(ftype::f64 progress) const { return final_temp + (init_temp - final_temp) / (1.0 + std::exp(steepness * (progress - 0.5))); }
 };
@@ -55,7 +50,6 @@ class LogarithmicTemp {
     ftype::f64 init_temp, final_temp;
     ftype::f64 steepness;
 public:
-    using result_type = ftype::f64;
     LogarithmicTemp(ftype::f64 init_temp, ftype::f64 final_temp = 0.0, ftype::f64 steepness = 10.0) : init_temp(init_temp), final_temp(final_temp), steepness(steepness) {}
     ftype::f64 operator()(ftype::f64 progress) const { return final_temp + (init_temp - final_temp) / (1.0 + std::log(steepness * (progress + 1e-6))); }
 };
@@ -64,7 +58,6 @@ class QuadraticTemp {
     ftype::f64 init_temp, final_temp;
     ftype::f64 steepness;
 public:
-    using result_type = ftype::f64;
     QuadraticTemp(ftype::f64 init_temp, ftype::f64 final_temp = 0.0, ftype::f64 steepness = 10.0) : init_temp(init_temp), final_temp(final_temp), steepness(steepness) {}
     ftype::f64 operator()(ftype::f64 progress) const { return final_temp + (init_temp - final_temp) / (1.0 + steepness * (progress - 0.5) * (progress - 0.5)); }
 };
@@ -73,7 +66,6 @@ class IterationProgress {
     itype::u32 max_iter;
     ftype::f64 current_progress = 0.0;
 public:
-    using result_type = ftype::f64;
     IterationProgress(itype::u32 max_iter) : max_iter(max_iter) {}
     ftype::f64 progress() const { return current_progress; }
     bool update(itype::u32 current_iter) {
@@ -87,7 +79,6 @@ class TimeProgress {
     std::chrono::system_clock::duration max_duration;
     ftype::f64 current_progress = 0.0;
 public:
-    using result_type = ftype::f64;
     TimeProgress(std::chrono::milliseconds max_duration) : start_time(std::chrono::system_clock::now()), max_duration(std::chrono::duration_cast<std::chrono::system_clock::duration>(max_duration)) {}
     ftype::f64 progress() { return current_progress; }
     bool update([[maybe_unused]] itype::u32 current_iter) {
@@ -105,10 +96,8 @@ namespace annealing {
 }  // namespace annealing
 
 template<class OptType, class TempFunction, class ProgressFunction> class Annealing {
-    static_assert(std::is_same<typename TempFunction::result_type, typename ProgressFunction::result_type>::value, "TempFunction::result_type and ProgressFunction::result_type must be the same type");
     static_assert(std::is_same<OptType, annealing::_Minimize>::value || std::is_same<OptType, annealing::_Maximize>::value, "Type must be OptimizeType::Minimize or OptimizeType::Maximize");
     constexpr static bool is_minimize = std::is_same<OptType, annealing::_Minimize>::value;
-    using value_type = typename TempFunction::result_type;
     TempFunction temp_function;
     ProgressFunction progress_function;
     ftype::f64 current_score;
@@ -118,21 +107,17 @@ template<class OptType, class TempFunction, class ProgressFunction> class Anneal
     itype::u32 ugap;
     itype::u32 current_iter = 0;
     bool is_best_updated = false;
-    [[no_unique_address]] Allocator<value_type> alloc;
-    value_type* rnd_buf = nullptr;
+    [[no_unique_address]] Allocator<ftype::f64> alloc;
+    ftype::f64* rnd_buf = nullptr;
     itype::u32 rnd_buf_size;
 public:
     Annealing() = delete;
     Annealing(const OptType&, const TempFunction& tempf, const ProgressFunction& progressf, ftype::f64 init_score, itype::u32 update_gap = 8, itype::u64 seed = Rand64::default_seed, itype::u32 buf_size = 12) : temp_function(tempf), progress_function(progressf), current_score(init_score), best_score(init_score), ugap(1u << update_gap), alloc(), rnd_buf_size(1u << buf_size) {
-        rnd_buf = alloc.allocate(rnd_buf_size);
-        Rand64 rand_function(seed);
-        if constexpr (sizeof(value_type) >= 8) {
+        if constexpr (!std::is_same<TempFunction, ZeroTemp>::value) {
+            rnd_buf = alloc.allocate(rnd_buf_size);
+            Rand64 rand_function(seed);
             for (itype::u32 i = 0; i < rnd_buf_size; ++i) {
-                rnd_buf[i] = std::log(1.0 - Canocicaled64(rand_function));
-            }
-        } else {
-            for (itype::u32 i = 0; i < rnd_buf_size; ++i) {
-                rnd_buf[i] = std::log(1.0f - Canocicaled32(rand_function));
+                rnd_buf[i] = std::log(1.0 - Canocicaled32(rand_function));
             }
         }
     }
@@ -148,12 +133,18 @@ public:
     bool update() {
         if (current_iter % ugap == 0) {
             if (!progress_function.update(current_iter)) return false;
-            current_temp = temp_function(progress_function.progress());
+            if constexpr (!std::is_same<TempFunction, ZeroTemp>::value) {
+                current_temp = temp_function(progress_function.progress());
+            }
         }
-        if constexpr (is_minimize) {
-            threshold_score = current_score - current_temp * rnd_buf[current_iter & (rnd_buf_size - 1)];
+        if constexpr (std::is_same<TempFunction, ZeroTemp>::value) {
+            threshold_score = current_score;
         } else {
-            threshold_score = current_score + current_temp * rnd_buf[current_iter & (rnd_buf_size - 1)];
+            if constexpr (is_minimize) {
+                threshold_score = current_score - current_temp * rnd_buf[current_iter & (rnd_buf_size - 1)];
+            } else {
+                threshold_score = current_score + current_temp * rnd_buf[current_iter & (rnd_buf_size - 1)];
+            }
         }
         ++current_iter;
         return true;
